e69b6a9d07653ddadc91fce3c513641ad388b0e7
LagranjGH <palozz@yandex.ru>
2015-11-16 11:08:49 +0300

    Рефакторинг теста

--- MyUtil/src/ru/paloz/test/ConvertersTest.java
+++ MyUtil/src/ru/paloz/test/ConvertersTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import ru.paloz.common.CollectionConverter;
 
-public class ConvertersTester {
+public class ConvertersTest {
 
 	@Test
 	public void testCollectionConverterInt() {
